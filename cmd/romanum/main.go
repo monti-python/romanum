35f5f221c54b7c5fd8ef024ba99db1326c6c3dc7
monti-python <32843378+monti-python@users.noreply.github.com>
2024-05-22 00:56:49 +0200

    Parse command line argument

--- cmd/romanum/main.go
+++ cmd/romanum/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Starting up romanum server...")
-	fmt.Println("Halted execution")
+	// fmt.Println("Starting up romanum server...")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: romanum <number>")
+		os.Exit(1)
+	}
+
+	input := os.Args[1]
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Printf("Error: Invalid number '%s'\n", input)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Input number is %d\n", number)
+	// fmt.Println("Halted execution")
 }
